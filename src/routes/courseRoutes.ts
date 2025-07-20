49cf2a2165ee1d9ab8c35f1c9e998268dd134f61
Chethan C K <chethanckm66@gmail.com>
2025-07-21 00:04:01 +0530

    Adding amazon s3 changes for video upload url

--- src/routes/courseRoutes.ts
+++ src/routes/courseRoutes.ts
@@ -6,6 +6,7 @@ import {
   listCourses,
   updateCourse,
   deleteCourse,
+  getUploadVideoUrl,
 } from '../controllers/courseController'
 import { requireAuth } from '@clerk/express'
 
@@ -18,5 +19,11 @@ router.post('/', requireAuth(), createCourse)
 router.get('/:courseId', getCourse)
 router.put('/:courseId', requireAuth(), upload.single('image'), updateCourse)
 router.delete('/:courseId', requireAuth(), deleteCourse)
+
+router.post(
+  '/:courseId/sections/:sectionId/chapters/:chapterId/get-upload-url',
+  requireAuth(),
+  getUploadVideoUrl
+)
 
 export default router
